ab84271842ec6b8441c6309c8ad47b451f32b81f
Alexander Bondarenco <sasha.199568@gmail.com>
2025-07-27 20:33:01 +0300

    print_vector -> print

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ struct Vector {
     double z;
 };
 
-void print_vector(Vector vector) {
+void print(Vector vector) {
     printf("Vector(%f %f %f)\n", vector.x, vector.y, vector.z);
 }
 
@@ -18,17 +18,23 @@ Vector operator-(Vector a, Vector b) {
     return Vector{a.x - b.x, a.y - b.y, a.z - b.z};
 }
 
+Vector operator*(Vector a, double d) {
+    return Vector{a.x * d, a.y * d, a.z * d};
+}
+
 int main() {
     printf("Cystem\n");
 
     Vector a = {1, 2, 3};
-    print_vector(a);
+    print(a);
     Vector b = {2, 3, 4};
-    print_vector(b);
+    print(b);
     auto sum = a + b;
-    print_vector(sum);
+    print(sum);
     auto dif = a - b;
-    print_vector(dif);
+    print(dif);
+    auto m = a * 2;
+    print(m);
 
     return 0;
 }
