67b3e0d520de165162e38b3f97b1766160eb830d
Alexander Bondarenco <sasha.199568@gmail.com>
2025-07-27 20:20:47 +0300

    Vector

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+typedef struct Vector {
+    double x;
+    double y;
+    double z;
+} Vector;
+
+void print_vector(Vector vector) {
+    printf("Vector(%f %f %f)\n", vector.x, vector.y, vector.z);
+}
+
+Vector vvadd(Vector a, Vector b) {
+    Vector result = {a.x + b.x, a.y + b.y, a.z + b.z};
+    return result;
+}
+
 int main() {
     printf("Cystem\n");
+
+    Vector a = {1, 2, 3};
+    print_vector(a);
+    Vector b = {2, 3, 4};
+    print_vector(b);
+    Vector sum = vvadd(a, b);
+    print_vector(sum);
 
     return 0;
 }
