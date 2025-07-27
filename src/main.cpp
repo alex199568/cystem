7d2a5f7d37b8408ca2130b46b61cc859b9e84ca8
Alexander Bondarenco <sasha.199568@gmail.com>
2025-07-27 21:15:18 +0300

    Point operators

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,17 @@ int main() {
     Vector b = {2, 3, 4};
     print(b);
 
-    Point p = {-1, 3, 2};
-    print(p);
+    Point p1 = {-1, 3, 2};
+    print(p1);
+    Point p2 = {2, -8, 3};
+    print(p2);
+
+    auto r1 = p1 + a;
+    print(r1);
+    auto r2 = p1 - b;
+    print(r2);
+    auto r3 = p1 - p2;
+    print(r3);
 
     return 0;
 }
--- src/point.hpp
+++ src/point.hpp
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+#include "vector.hpp"
+
 struct Point {
     double x;
     double y;
@@ -11,6 +13,18 @@ struct Point {
 
 void print(Point p) {
     printf("Point(%f, %f, %f)\n", p.x, p.y, p.z);
+}
+
+Point operator+(Point p, Vector v) {
+    return Point{p.x + v.x, p.y + v.y, p.z + v.z};
+}
+
+Point operator-(Point p, Vector v) {
+    return Point{p.x - v.x, p.y - v.y, p.z - v.z};
+}
+
+Vector operator-(Point a, Point b) {
+    return Vector{a.x - b.x, a.y - b.y, a.z - b.z};
 }
 
 #endif
