807a1aa0f714e06748003d550d6e1fecaae222b2
Alexander Bondarenco <sasha.199568@gmail.com>
2025-08-03 19:01:46 +0300

    Matrix classes

--- src/main.cpp
+++ src/main.cpp
@@ -24,8 +24,14 @@ int main() {
 
     auto tr = translation(1, 2.0, -2.3);
     auto s = scale(0.4, 0.2, 4);
+
+    tr.print();
+    printf("\n");
+    s.print();
+    printf("\n");
+
     auto result = tr * s;
-    print(result);
+    result.print();
 
     return 0;
 }
--- src/matrix.hpp
+++ src/matrix.hpp
@@ -5,61 +5,126 @@
 
 #include <array>
 
-typedef std::array<double, 4> matrix2;
-typedef std::array<double, 9> matrix3;
-typedef std::array<double, 16> matrix;
+class Matrix2 {
+    std::array<double, 4> items;
 
-int index2(int i, int j) {
-    return i * 2 + j;
-}
-
-int index3(int i, int j) {
-    return i * 3 + j;
-}
-
-int index(int i, int j) {
-    return i * 4 + j;
-}
-
-void print(matrix items) {
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            printf("%f ", items[index(i, j)]);
-        }
-        printf("\n");
+    int index(int i, int j) {
+        return i * 2 + j;
     }
-}
 
-matrix operator*(matrix a, matrix b) {
-    matrix result;
+    double get(int i, int j) {
+        return items[index(i, j)];
+    }
+
+    void set(int i, int j, double d) {
+        items[index(i, j)] = d;
+    }
+
+  public:
+    Matrix2() {}
+
+    Matrix2(std::array<double, 4> values) : items(values) {}
+
+    void print() {
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                printf("%6.3f ", get(i, j));
+            }
+            printf("\n");
+        }
+    }
+};
+
+class Matrix3 {
+
+    std::array<double, 9> items;
+
+    int index(int i, int j) {
+        return i * 3 + j;
+    }
+
+    double get(int i, int j) {
+        return items[index(i, j)];
+    }
+
+    void set(int i, int j, double d) {
+        items[index(i, j)] = d;
+    }
+
+  public:
+    Matrix3() {}
+
+    Matrix3(std::array<double, 9> values) : items(values) {}
+
+    void print() {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                printf("%6.3f ", get(i, j));
+            }
+            printf("\n");
+        }
+    }
+};
+
+class Matrix {
+
+    std::array<double, 16> items;
+
+    int index(int i, int j) {
+        return i * 4 + j;
+    }
+
+  public:
+    Matrix() {}
+
+    Matrix(std::array<double, 16> values) : items(values) {}
+
+    double get(int i, int j) {
+        return items[index(i, j)];
+    }
+
+    void set(int i, int j, double d) {
+        items[index(i, j)] = d;
+    }
+
+    void print() {
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                printf("%6.3f ", get(i, j));
+            }
+            printf("\n");
+        }
+    }
+};
+
+Matrix operator*(Matrix a, Matrix b) {
+    Matrix result;
 
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
             double s = 0;
             for (int k = 0; k < 4; ++k) {
-                s += a[index(i, k)] * b[index(k, j)];
+                s += a.get(i, k) * b.get(k, j);
             }
-            result[index(i, j)] = s;
+            result.set(i, j, s);
         }
     }
 
     return result;
 }
 
-matrix translation(double x, double y, double z) {
-    return {
-        1, 0, 0, x,
-        0, 1, 0, y,
-        0, 0, 1, z,
-        0, 0, 0, 1};
+Matrix translation(double x, double y, double z) {
+    return Matrix({1, 0, 0, x,
+                   0, 1, 0, y,
+                   0, 0, 1, z,
+                   0, 0, 0, 1});
 }
 
-matrix scale(double x, double y, double z) {
-    return {
-        x, 0, 0, 0,
-        0, y, 0, 0,
-        0, 0, z, 0,
-        0, 0, 0, 1};
+Matrix scale(double x, double y, double z) {
+    return Matrix({x, 0, 0, 0,
+                   0, y, 0, 0,
+                   0, 0, z, 0,
+                   0, 0, 0, 1});
 }
 
 #endif
