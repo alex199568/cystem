1342fecebf0cee9471e42e8d03960464dfdda15d
Alexander Bondarenco <sasha.199568@gmail.com>
2025-08-03 14:15:03 +0300

    Image

---
+++ src/image.hpp
@@ -0,0 +1,32 @@
+#ifndef _IMAGE_HPP_
+#define _IMAGE_HPP_
+
+#include "color.hpp"
+
+struct Image {
+    int w;
+    int h;
+    Color *colors;
+
+    Image(int w, int h) : w(w), h(h) {
+        colors = new Color[w * h];
+    }
+
+    ~Image() {
+        delete colors;
+    }
+
+    int index(int x, int y) {
+        return y * w + x;
+    }
+
+    Color get(int x, int y) {
+        return colors[index(x, y)];
+    }
+
+    void set(int x, int y, Color color) {
+        colors[index(x, y)] = color;
+    }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 #include "vector.hpp"
 #include "point.hpp"
 #include "color.hpp"
+#include "image.hpp"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 int main() {
     printf("Cystem\n");
 
-    print(white);
-    print(red);
-    print(magenta);
-    print(cyan);
+    Image image(128, 96);
+
+    for (int y = 10; y < 30; ++y) {
+        for (int x = 20; x < 80; ++x) {
+            image.set(x, y, red);
+        }
+    }
+
+    uint8_t *data = new uint8_t[image.w * image.h * 3];
+    for (int y = 0; y < image.h; ++y) {
+        for (int x = 0; x < image.w; ++x) {
+            int i = y * image.w + x;
+            data[i * 3 + 0] = (uint8_t)(fmin(fmax(image.colors[i].r * 255.0, 0.0), 255.0));
+            data[i * 3 + 1] = (uint8_t)(fmin(fmax(image.colors[i].g * 255.0, 0.0), 255.0));
+            data[i * 3 + 2] = (uint8_t)(fmin(fmax(image.colors[i].b * 255.0, 0.0), 255.0));
+        }
+    }
+
+    stbi_write_png("renders/image.png", image.w, image.h, 3, data, image.w * 3);
 
     return 0;
 }
